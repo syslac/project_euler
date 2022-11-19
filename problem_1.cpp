186e1e50d794f0ec492326ba191bd774e389ee19
syslac <lmezzalira@gmail.com>
2022-11-19 19:16:48 +0100

    Project Euler: problem 1

---
+++ problem_1.cpp
@@ -0,0 +1,65 @@
+#include <chrono>
+#include <iostream>
+
+const int limitNumber = 1000;
+const int block = 30;
+
+bool useForSum(int n)
+{
+    const int numDividends = 2;
+    int dividends[numDividends];
+    dividends[0] = 3;
+    dividends[1] = 5;
+    for (int i = 0; i < numDividends; i++)
+    {
+       if (n % dividends[i] == 0)                   
+       {
+           return true;
+       }
+    }
+    return false;
+}
+
+std::pair<int, int> sumOfBlock() 
+{
+    int sum = 0;
+    int count = 0;
+    for (int i = 0; i < block; i++) 
+    {
+        if (useForSum(i))
+        {
+            sum += i;
+            count++;
+        }
+    }
+    return std::pair<int, int> (sum, count);
+}
+
+int main() 
+{
+    auto startTm = std::chrono::high_resolution_clock::now();
+    
+    std::pair<int, int> basicBlockSum = sumOfBlock();
+    long long runningSum = 0;
+    int latestBlock = 0;
+    // sum known blocks
+    for (int i = 0; i + block < limitNumber; i+= block) 
+    {
+        runningSum += basicBlockSum.first;
+        runningSum += basicBlockSum.second * i; 
+        latestBlock = i;
+    }
+    // handle leftOver
+    for (int i = latestBlock; i < limitNumber; i++) 
+    {
+        if (useForSum(i))                   
+        {
+            runningSum += i;
+        }
+    } 
+
+    auto endTm = std::chrono::high_resolution_clock::now();
+    auto microseconds = std::chrono::duration_cast<std::chrono::nanoseconds>(endTm-startTm);
+    std::cout << "Solution: " << runningSum << std::endl;
+    std::cout << "Timing: " << microseconds.count() << "ns\n";
+}
---
+++ problem_1.md
@@ -0,0 +1,10 @@
+### Problem [https://projecteuler.net/problem=1](https://projecteuler.net/problem=1)
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+### Solution
+
+Key is recognizing the pattern will repeat every 30 numbers. If you know the sum of these multiples from 0 to 30, and how many addends it's composed of, you can just sum "block-of-30s".
+
